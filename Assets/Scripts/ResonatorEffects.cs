f6bcbb52e89e42e16b74425c8de0ffc0c5f275ae
uncle-draza <dilpdrazovic@gmail.com>
2023-05-22 02:44:13 +0200

    dodati post procesing efekti na rezonator, optimizovane teksture, obrisani nepotrebni skyboxevi

--- Assets/Scripts/ResonatorEffects.cs
+++ Assets/Scripts/ResonatorEffects.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class ResonatorEffects : MonoBehaviour
 {
@@ -14,11 +16,20 @@ public class ResonatorEffects : MonoBehaviour
     public float lightMax;
     public Light light1;
     public Light light2;
-
+    public Transform player;
+    public float ppEffectMaxDistance;
+    private float distance;
+    private float mappedValue;
+    public Volume volume;
+    FilmGrain filmGrain;
+    ChromaticAberration chromaticAberration;
+    public Transform anchorPoint;
     
     void Start()
     {
         emissiveMaterial = objectToChange.GetComponent<Renderer>().material;
+        volume.profile.TryGet<FilmGrain>(out filmGrain);
+        volume.profile.TryGet<ChromaticAberration>(out chromaticAberration);
     }
 
 
@@ -35,6 +46,19 @@ public class ResonatorEffects : MonoBehaviour
         SetEmissionIntensity(value);
         light1.intensity =lightvalue;
         light2.intensity = lightvalue;
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        distance = Vector3.Distance(player.position, anchorPoint.position);
+        Debug.Log(distance);
+        mappedValue = Mathf.InverseLerp(0f, ppEffectMaxDistance, distance);
+        mappedValue = Mathf.Clamp01(mappedValue);
+        mappedValue = 1f - mappedValue;
+        filmGrain.intensity.value = mappedValue;
+        chromaticAberration.intensity.value = mappedValue / 2;
     }
 
     public void SetEmissionIntensity(float value)
